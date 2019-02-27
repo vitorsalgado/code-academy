8e800dcdb9ac60ad6f04a21e9bbf4b0819bec1e4
Vitor Salgado <vsalgadopb@gmail.com>
2019-02-26 23:57:42 -0300

    gradle

--- training/sample/evaluate-training.js
+++ training/sample/evaluate-training.js
@@ -3,6 +3,6 @@
 const Spawn = require('child_process').spawn
 const SHELL_OPTIONS = { stdio: 'inherit', shell: true }
 
-const evaluateTraining = () => Spawn('./gradlew', ['check', '--stacktrace', '--no-build-cache'], SHELL_OPTIONS)
+const evaluateTraining = () => Spawn('gradle', ['check', '--stacktrace'], SHELL_OPTIONS)
 
 evaluateTraining()
