3ac336e6cca259f7dcfc0f1897baa307f2083f3e
Vitor Salgado <vsalgadopb@gmail.com>
2019-02-28 13:14:42 -0300

    exit code

--- training/sample/evaluate-training.js
+++ training/sample/evaluate-training.js
@@ -3,6 +3,8 @@
 const Spawn = require('child_process').spawn
 const SHELL_OPTIONS = { stdio: 'inherit', shell: true }
 
-const evaluateTraining = () => Spawn('gradle', ['check', '--stacktrace'], SHELL_OPTIONS)
+const evaluateTraining = () =>
+  Spawn('gradle', ['check', '--stacktrace'], SHELL_OPTIONS)
+    .on('exit', process.exit)
 
 evaluateTraining()
