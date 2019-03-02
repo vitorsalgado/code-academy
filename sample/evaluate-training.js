df139a81c67bce035c52159e42a0323d6787266b
Vitor Salgado <vsalgadopb@gmail.com>
2019-03-02 00:52:11 -0300

    gradle

--- sample/evaluate-training.js
+++ sample/evaluate-training.js
@@ -4,7 +4,7 @@ const Spawn = require('child_process').spawn
 const SHELL_OPTIONS = { stdio: 'inherit', shell: true }
 
 const evaluateTraining = () =>
-  Spawn('./gradle', ['check', '--error'], SHELL_OPTIONS)
+  Spawn('./gradlew', ['check', '--error'], SHELL_OPTIONS)
     .on('exit', process.exit)
 
 evaluateTraining()
