06f2a964167b6c579de6dd9d1cd84649bc2ec1c8
Vitor Salgado <vsalgadopb@gmail.com>
2019-03-02 01:59:05 -0300

    mvn to gradle

---
+++ maven_to_gradle/src/main/java/com/example/MavenToGradleApp.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class MavenToGradleApp {
+
+  public static void main(final String[] args) {
+    SpringApplication.run(MavenToGradleApp.class, args);
+  }
+}
